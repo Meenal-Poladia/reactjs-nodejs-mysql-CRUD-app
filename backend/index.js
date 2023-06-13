cc97336064ed1e7b36adb3f50871d4fe3f121221
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-06-13 11:07:23 -0400

    Connected backend with database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
 
+export const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'blog'
+})
+
 app.use(express.json());
+
+app.get("/", (request, response) => {
+    response.json("Hello from backedn");
+})
 
 app.listen(8000, () => {
     console.log('Connected');
