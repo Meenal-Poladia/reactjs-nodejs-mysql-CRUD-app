640de781988786514f696ddb7247abf3e2fef17d
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-06-13 11:44:08 -0400

    Fetching the data about all the books from the db

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,22 @@ const app = express();
 export const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
-    database: 'blog'
+    password: 'mysqlroot',
+    database: 'test'
 })
 
 app.use(express.json());
 
 app.get("/", (request, response) => {
     response.json("Hello from backedn");
+})
+
+app.get("/books", (request, response) => {
+    const query = "SELECT * FROM books";
+    db.query(query, (error, data) => {
+        if (error) response.json("There is an error");
+        return response.json(data);
+    })
 })
 
 app.listen(8000, () => {
